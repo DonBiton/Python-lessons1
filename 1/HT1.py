cb691d1075507459f530c642d7e10fbf891126f1
Don Biton <miha1750@yandex.ru>
2024-09-11 00:04:18 +0300

    HT1 done

--- 1/HT1.py
+++ 1/HT1.py
@@ -1,5 +1,16 @@
 import math
 #1
-r = float(input('Введите R:'))
-print('Площадь круга: '+ str(round(math.pi*r**2,2)))
-print('Площадь круга: '+ str(round(math.pi*r**2,2)))
+# r = float(input('Введите R:'))
+# print('Площадь круга: '+ str(round(math.pi*r**2,2)))
+# print('Длина окружности: '+ str(round(2*math.pi*r,2)))
+
+#2
+# x, y = 10, 55
+# print(x,y)
+# x, y = y, x
+# print(x,y)
+
+#3
+g = 9.81
+l=float(input('Введите Z '))
+print('T =', round(2*math.pi*math.sqrt(l/g),2))
