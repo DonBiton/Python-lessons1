889a9358908046e0f70e23dc3f5728a18269d70a
Don Biton <miha1750@yandex.ru>
2024-11-17 20:47:07 +0300

    HT9.py

--- 1/pythonchik.py
+++ 1/pythonchik.py
@@ -1,3 +1,14 @@
-t=float(input('t= '))
-x=float(input('x= '))
-print(t)
+import matplotlib.pyplot as plt
+import numpy as np
+def f(x, alpha, beta):
+    a = alpha
+    b = beta
+    return (x**b)
+fig = plt.figure(dpi=200)
+plt.title("Исследование заданной функции - 2")
+ax = fig.add_subplot()
+x = np.linspace(-50,50, 100)
+ax.plot(x,f(x,2,2))
+ax.grid()
+
+plt.show()
